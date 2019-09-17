a0f70b0e7f8f7ee23195b0c52025085fea2b46ed
Jesray Garciano <jesray.garciano@gmail.com>
2019-09-17 12:50:51 +0800

    Routes modified

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/wati', 'DiaryController@index')
+Route::get('/wati', 'DairyController@index')
     ->name('dairy.wati');
